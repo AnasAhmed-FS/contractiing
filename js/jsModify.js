1d2d1f05ca1153c3dbbbf06acaa0301605b1243d
AnasAhmed-FS <75071797+AnasAhmed-FS@users.noreply.github.com>
2021-08-04 21:53:35 +0200

    Fourth

--- js/jsModify.js
+++ js/jsModify.js
@@ -1,5 +1,4 @@
-// NavPar Sticky and NavPar Fixed
-
+// NavPar Sticky and NavPar Fixed 
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
@@ -14,3 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+// //////////////////////////////////
+// create move in  intro website  
+count = 0;
+words = "Welcome, WE ARE CONSTRUCTED, This's our website for <b> EXCELLENCE PALACE COMPANY </b> ,  we are  nice to meet you and we hope to have s a good futuer with us "
+    // to have set with loop ----> setTimeout 
+setInterval(function() {
+    dif = document.getElementById("text_dif");
+    dif.innerHTML = words.substring(0, count);
+    count++;
+    /* set with loop--->   
+    if (count > words.length) {
+        count = 0;
+    }
+    */
+}, 50)
