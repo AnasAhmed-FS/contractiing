851145e041ce8f94431c10f2aef5d8ae57ba5a86
Anas Ahmed <Anas.Ahmed@CEGDoha.Local>
2021-08-10 12:41:15 +0300

    16

--- js/contractingmodify.js
+++ js/contractingmodify.js
@@ -15,35 +15,35 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-// //////////////////////////////////
-// create move in  intro website  
-count = 0;
-words = "Welcome, WE ARE CONSTRUCTED, This's our website for <b> EXCELLENCE PALACE COMPANY </b> ,  we are  nice to meet you and we hope to have s a good futuer with us "
-    // to have set with loop ----> setTimeout 
-setInterval(function() {
-    dif = document.getElementById("text_dif");
-    dif.innerHTML = words.substring(0, count);
-    count++;
-    /* set with loop--->   
-    if (count > words.length) {
-        count = 0;
-    }
-    */
-}, 50);
+// // //////////////////////////////////
+// // create move in  intro website  
+// count = 0;
+// words = "Welcome, WE ARE CONSTRUCTED, This's our website for <b> EXCELLENCE PALACE COMPANY </b> ,  we are  nice to meet you and we hope to have s a good futuer with us "
+//     // to have set with loop ----> setTimeout 
+// setInterval(function() {
+//     dif = document.getElementById("text_dif");
+//     dif.innerHTML = words.substring(0, count);
+//     count++;
+//     /* set with loop--->   
+//     if (count > words.length) {
+//         count = 0;
+//     }
+//     */
+// }, 50);
 
-//  company video   comp_vid_explination
-counts = 0;
-comp_vid = "Here you will find our Video Excplination and youl will a good idaes from it . Enjoy Waching ^-^  ";
-// to have set with loop ----> setTimeout 
-setInterval(function() {
-    vid = document.getElementById("comp_vid_explination");
-    vid.innerHTML = comp_vid.substring(0, counts);
-    counts++;
-    if (counts > comp_vid.length) {
-        counts = 0;
-    }
+// //  company video   comp_vid_explination
+// counts = 0;
+// comp_vid = "Here you will find our Video Excplination and youl will a good idaes from it . Enjoy Waching ^-^  ";
+// // to have set with loop ----> setTimeout 
+// setInterval(function() {
+//     vid = document.getElementById("comp_vid_explination");
+//     vid.innerHTML = comp_vid.substring(0, counts);
+//     counts++;
+//     if (counts > comp_vid.length) {
+//         counts = 0;
+//     }
 
-}, 100);
+// }, 100);
 
 // services
 var myCarousel = document.querySelector('#carouselProject')
